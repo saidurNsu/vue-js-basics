b3a25cee4a2a3eeaac3d199bbdb882d820d7c406
saidurNsu <37741326+saidurNsu@users.noreply.github.com>
2020-09-09 12:22:59 +0600

    Add files via upload

---
+++ vue_component.js
@@ -0,0 +1,9 @@
+Vue.component('testcomponent',{
+   template : '<div><h1>welcome from vue js component</h1></div>'
+});
+var vm = new Vue({
+   el: '#component_saidur'
+});
+var vm1 = new Vue({
+   el: '#component_rahman'
+});
