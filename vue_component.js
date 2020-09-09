bd3fc5d9dc39ee5087734c9cb825cdeac783c7b4
saidurNsu <37741326+saidurNsu@users.noreply.github.com>
2020-09-09 13:00:46 +0600

    Add files via upload

--- vue_component.js
+++ vue_component.js
@@ -1,6 +1,29 @@
 Vue.component('testcomponent',{
    template : '<div><h1>welcome from vue js component</h1></div>'
 });
+
+Vue.component('testcomponentsaidur',{
+   template : '<div v-on:mouseover = "changename()" v-on:mouseout = "originalname();"><h1>Custom Component created by <span id = "name">{{name}}</span></h1></div>',
+   data: function() {
+      return {
+         name : "sabbir"
+      }
+   },
+   methods:{
+      changename : function() {
+         this.name = "Sakib Al hassan";
+      },
+      originalname: function() {
+         this.name = "sabbir";
+      }
+   }
+});
+
+var vm0 = new Vue({
+   el: '#component_test_saidur'
+});
+
+
 var vm = new Vue({
    el: '#component_saidur'
 });
